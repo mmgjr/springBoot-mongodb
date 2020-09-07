899369b843931c2416ac228fcece14d2bf2414a3
Mucio M G Junior <mlk_mucio@hotmail.com>
2020-09-07 11:33:48 -0300

    Projeto criado

---
+++ src/test/java/com/muciomgjr/learningmongo/LearningmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muciomgjr.learningmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
