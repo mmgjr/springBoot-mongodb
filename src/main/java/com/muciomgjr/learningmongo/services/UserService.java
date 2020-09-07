92ba40d79089dbf85aa420a909150409d37e6f8d
Mucio M G Junior <mlk_mucio@hotmail.com>
2020-09-07 15:22:26 -0300

    Usuário delete e insert

--- src/main/java/com/muciomgjr/learningmongo/services/UserService.java
+++ src/main/java/com/muciomgjr/learningmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.muciomgjr.learningmongo.domain.User;
+import com.muciomgjr.learningmongo.dto.UserDTO;
 import com.muciomgjr.learningmongo.repository.UserRepository;
 import com.muciomgjr.learningmongo.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,18 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+	
+	public void delete(String id) {
+		findById(id);
+		repository.deleteById(id);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
